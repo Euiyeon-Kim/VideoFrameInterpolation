253ca5f30b738004dca2dbe826a6e77ec373eae8
Euiyeon-Kim <keywi9811key@gmail.com>
2023-03-15 18:06:45 +0900

    DCNTransv1_swinV2_shareDCNatD4_enc5dec10_GeoF32_Distill_halfTonly

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
-NUM_GPUS=1
+NUM_GPUS=4
 
 python -m torch.distributed.launch \
 --nproc_per_node=${NUM_GPUS} \
 train.py \
---exp_name DCNTrans/DCNTransv1_sepDCNatD4_enc5dec10_GeoF3_Distill_halfTonly \
+--exp_name DCNTrans/DCNTransv1_swinV2_shareDCNatD4_enc5dec10_GeoF32_Distill_halfTonly \
 --config configs/DCNTrans.yaml \
 --world_size ${NUM_GPUS}
